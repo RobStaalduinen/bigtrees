eb73d958e5ea9867869c0a408bd5ad9514f16c7b
RobStaalduinen <rob.staalduinen@gmail.com>
2018-03-14 16:42:25 -0400

    fix commercial image links

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 0) do
+ActiveRecord::Schema.define(version: 20180314161346) do
 
   create_table "appointments", force: :cascade do |t|
     t.integer "estimate_id",    limit: 4
