31d44ef37965dd19a3ba7bb47d5ea149cfae9e0a
RobStaalduinen <rob.staalduinen@gmail.com>
2018-04-23 15:43:46 -0400

    fix tree image upload

--- app/models/tree_image.rb
+++ app/models/tree_image.rb
@@ -5,8 +5,8 @@ attr_accessible *column_names
 
   has_attached_file :asset, :path =>  "images/:rails_env/:class/:image_name.:content_type_extension"
 
-  def image_name
-    "Esimate_#{self.estimate_id}_Tree_#{self.tree_number}_#{Random.new.rand(10000)}"
+  Paperclip.interpolates :image_name do |attachment, style|
+    "Esimate_#{attachment.instance.estimate_id}_Tree_#{attachment.instance.tree_number}_Image_#{attachment.instance.id}"
   end
 
   validates_attachment_content_type :asset, :content_type => ["image/jpg", "image/jpeg", "image/png", "image/gif"]
