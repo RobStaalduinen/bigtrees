63825c8fe560fb65bd685481465c20be9f9934ff
RobStaalduinen <rob.staalduinen@gmail.com>
2018-04-24 12:13:33 -0400

    use sendinblue

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -84,23 +84,23 @@ Rails.application.configure do
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.raise_delivery_errors = true
 
-  # config.action_mailer.smtp_settings = {
-  #   :address => 'smtp-relay.sendinblue.com',
-  #   :port => 587,
-  #   :domain => 'bigtreeservices.com',
-  #   :user_name => ENV["EMAIL_USERNAME"],
-  #   :password => ENV["EMAIL_PASSWORD"],
-  #   :authentication => 'login',
-  #   :enable_starttls_auto => true
-  # }
-
   config.action_mailer.smtp_settings = {
-    address:              'smtp.gmail.com',
-    port:                 587,
-    domain:               'bigtreeservices.com',
-    user_name:            ENV["EMAIL_USERNAME"],
-    password:             ENV["EMAIL_PASSWORD"],
-    authentication:       'plain',
-    enable_starttls_auto: true
+    :address => 'smtp-relay.sendinblue.com',
+    :port => 587,
+    :domain => 'bigtreeservices.com',
+    :user_name => ENV["EMAIL_USERNAME"],
+    :password => ENV["EMAIL_PASSWORD"],
+    :authentication => 'login',
+    :enable_starttls_auto => true
   }
+
+  # config.action_mailer.smtp_settings = {
+  #   address:              'smtp.gmail.com',
+  #   port:                 587,
+  #   domain:               'bigtreeservices.com',
+  #   user_name:            ENV["EMAIL_USERNAME"],
+  #   password:             ENV["EMAIL_PASSWORD"],
+  #   authentication:       'plain',
+  #   enable_starttls_auto: true
+  # }
 end
