893c23f4fc385c1629d56f7f4d0ed93fc8e07ddb
RobStaalduinen <rob.staalduinen@gmail.com>
2018-04-24 11:29:36 -0400

    gmail test

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -43,6 +43,8 @@ Rails.application.configure do
 
   config.action_mailer.default_url_options = { :host => "bigtreeservices.com" }
 
+  config.action_mailer.perform_deliveries = true
+
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
     :address => 'smtp-relay.sendinblue.com',
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -77,16 +77,30 @@ Rails.application.configure do
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
 
+  config.action_mailer.perform_deliveries = true
+
   config.action_mailer.default_url_options = { :host => "bigtreeservices.com" }
 
   config.action_mailer.delivery_method = :smtp
+  config.action_mailer.raise_delivery_errors = false
+
+  # config.action_mailer.smtp_settings = {
+  #   :address => 'smtp-relay.sendinblue.com',
+  #   :port => 587,
+  #   :domain => 'bigtreeservices.com',
+  #   :user_name => ENV["EMAIL_USERNAME"],
+  #   :password => ENV["EMAIL_PASSWORD"],
+  #   :authentication => 'login',
+  #   :enable_starttls_auto => true
+  # }
+
   config.action_mailer.smtp_settings = {
-    :address => 'smtp-relay.sendinblue.com',
-    :port => 587,
-    :domain => 'bigtreeservices.com',
-    :user_name => ENV["EMAIL_USERNAME"],
-    :password => ENV["EMAIL_PASSWORD"],
-    :authentication => 'login',
-    :enable_starttls_auto => true
+    address:              'smtp.gmail.com',
+    port:                 587,
+    domain:               'bigtreeservices.com',
+    user_name:            ENV["EMAIL_USERNAME"],
+    password:             ENV["EMAIL_PASSWORD"],
+    authentication:       'plain',
+    enable_starttls_auto: true
   }
 end
