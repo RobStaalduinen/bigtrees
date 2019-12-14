290feaa086fc1334660dcafc95aed8b02d679c9f
RobStaalduinen <rob@stagelink.com>
2019-12-14 17:37:18 +0100

    fix customer

--- db/migrate/20191201111820_create_customer.rb
+++ db/migrate/20191201111820_create_customer.rb
@@ -1,11 +1,11 @@
 class CreateCustomer < ActiveRecord::Migration
   def change
-    create_table :customers do |t|
-      t.string :name
-      t.string :email
-      t.string :phone
-      t.string :preferred_contact
-    end
+    # create_table :customers do |t|
+    #   t.string :name
+    #   t.string :email
+    #   t.string :phone
+    #   t.string :preferred_contact
+    # end
 
     add_column :estimates, :customer_id, :integer, index: true
   end
