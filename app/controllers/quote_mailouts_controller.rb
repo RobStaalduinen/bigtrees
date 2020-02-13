22fe060640c7df6cab4c63859273fa4a58e7b7c3
RobStaalduinen <rob@stagelink.com>
2020-02-13 11:53:26 +0100

    refactor quote mailouts

--- app/controllers/quote_mailouts_controller.rb
+++ app/controllers/quote_mailouts_controller.rb
@@ -5,28 +5,32 @@ class QuoteMailoutsController < ApplicationController
 
   def new
     @estimate = Estimate.find(params[:estimate_id])
-    @is_final = params[:is_final] == 'true'
 
-    @mail_type = params[:mail_type]
+    render template: "quote_mailouts/new/#{params[:mail_type]}"
   end
 
   def create
     @estimate = Estimate.find(params[:estimate_id])
+    @estimate.customer.update(email: params[:dest_email]) if params[:update_email]
+    send_email unless params[:skip]
+    @estimate.update(estimate_params)
 
-    QuoteMailout.post_process_for_type(params[:mail_type], @estimate)
-
-    unless params[:skip]
-      if @estimate.customer.email.blank? && params[:mail_type] != QuoteMailout::MAIL_TYPES[:team]
-        @estimate.customer.update(email: params[:dest_email])
-      end
-
-      QuoteMailer.quote_email(@estimate, params[:dest_email], params[:subject], params[:content]).deliver_now
-    end
-
-    if params[:mail_type] == QuoteMailout::MAIL_TYPES[:followup]
-      redirect_to estimates_path
-    else
-      redirect_to estimate_path(@estimate)
-    end
+    redirect_to (params[:redirect] || estimate_path(@estimate))
   end
+
+  private
+
+    
+    def send_email
+      QuoteMailer.quote_email(
+        @estimate,
+        params[:dest_email],
+        params[:subject],
+        params[:content]
+    ).deliver_now
+    end
+
+    def estimate_params
+      params.permit(:quote_sent_date, :is_unknown)
+    end
 end
