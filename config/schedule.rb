15e43f3e15fb0da18265ff7a9b2a2303f6dc9132
RobStaalduinen <rob@stagelink.com>
2020-05-09 12:56:47 +0200

    fix schedule

--- config/schedule.rb
+++ config/schedule.rb
@@ -23,5 +23,5 @@ job_type :bin, 'cd :path/bin && :task'
 
 
 every 3.hours do
-  command "cd :path && $HOME/.rbenv/bin/rbenv exec delayed_job -n 2 restart"
+  command "cd /var/www/bigtrees/current && $HOME/.rbenv/bin/rbenv exec delayed_job -n 2 restart"
 end
