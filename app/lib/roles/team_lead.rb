2476f6093f300f0eb1310a111ba4b4f12d4d4b45
Rob van Staalduinen <rob.staalduinen@gmail.com>
2021-05-19 20:46:29 +0200

    Fix tema lead role

--- app/lib/roles/team_lead.rb
+++ app/lib/roles/team_lead.rb
@@ -3,7 +3,7 @@
 module Roles
   class TeamLead < Base
     def role_permissions
-      Roles::Arborist.role_permissions.merge(
+      Roles::Arborist.new.role_permissions.merge(
         {
           estimates: permission_set(list: true, show: true, update: true),
           customers: permission_set(show: true, create: true, update: true)
