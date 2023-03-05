a98f87e6b27a9e74c1c547c13c6a7a87d5ab1efc
Rob van Staalduinen <rob.staalduinen@gmail.com>
2023-03-05 13:22:56 -0500

    Allow mechanics to submit receipts

--- app/lib/roles/mechanic.rb
+++ app/lib/roles/mechanic.rb
@@ -6,7 +6,8 @@ module Roles
       {
         hours: permission_set(list: true, show: true, update: true),
         equipment_requests: permission_set(list: true, show: true, create: false, update: true, scope_level: 'organization'),
-        arborists: permission_set(show: true, update: true)
+        arborists: permission_set(show: true, update: true),
+        receipts: permission_set(list: true, show: true, create: true)
       }
     end
   end
