2e31402f7b11743e71fde0cc223a6f2bea25515a
Rob van Staalduinen <rob.staalduinen@gmail.com>
2026-04-30 15:47:09 -0400

    Add basic address autocomplete

--- app/javascript/components/createEstimate/singleAddressForm.vue
+++ app/javascript/components/createEstimate/singleAddressForm.vue
@@ -1,5 +1,13 @@
 <template>
   <div>
+    <b-form-group label="Find an Address">
+      <b-form-input
+        ref="findAddress"
+        placeholder="Start typing an address..."
+        @keydown.enter.prevent
+      ></b-form-input>
+    </b-form-group>
+
     <app-input-field
       v-model='street'
       :name='name + "street"'
@@ -27,7 +35,8 @@ export default {
   data(){
     return {
       street: null,
-      city: null
+      city: null,
+      autocomplete: null
     }
   },
   computed: {
@@ -52,11 +61,40 @@ export default {
         this.street = this.initialAddress.street;
         this.city = this.initialAddress.city;
       }
+    },
+    initAutocomplete() {
+      if (!window.google?.maps?.places) return;
+      const input = this.$refs.findAddress.$el;
+      if (!input) return;
+      this.autocomplete = new window.google.maps.places.Autocomplete(input, {
+        componentRestrictions: { country: 'ca' },
+        fields: ['address_components'],
+        types: ['address']
+      });
+      this.autocomplete.addListener('place_changed', this.onPlaceChanged);
+    },
+    onPlaceChanged() {
+      const place = this.autocomplete.getPlace();
+      if (!place.address_components) return;
+      const get = (type, nameType = 'long_name') => {
+        const c = place.address_components.find(c => c.types.includes(type));
+        return c ? c[nameType] : '';
+      };
+      this.street = [get('street_number', 'short_name'), get('route')].filter(Boolean).join(' ');
+      const cityTypes = ['locality', 'sublocality_level_1', 'sublocality', 'administrative_area_level_3', 'administrative_area_level_2'];
+      this.city = cityTypes.reduce((found, type) => found || get(type), '');
+      this.$refs.findAddress.$el.value = '';
     }
   },
   mounted() {
     this.updateInitialAddress();
     this.$emit('addressChange', this.address);
+    this.$nextTick(this.initAutocomplete);
+  },
+  beforeDestroy() {
+    if (this.autocomplete) {
+      window.google.maps.event.clearInstanceListeners(this.autocomplete);
+    }
   }
 
 }
